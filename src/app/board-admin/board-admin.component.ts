c638201d7e35f172d57cc98add4099b3160d6777
Roman Sulymka <roma.sulimak@gmail.com>
2021-05-07 09:35:26 +0300

    add update user at the admin page

--- src/app/board-admin/board-admin.component.ts
+++ src/app/board-admin/board-admin.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, Inject, ViewEncapsulation } from '@angular/core';
 import { UserService } from '../user-page/user.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator'
 import { User } from '../user-page/user';
 import { HttpErrorResponse } from '@angular/common/http';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+
+interface Role{
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-board-admin',
@@ -13,14 +20,20 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./board-admin.component.css']
 })
 export class BoardAdminComponent implements OnInit, AfterViewInit {
-  public displayedColumns = ['username', 'email', 'role', 'update', 'delete'];
+  id: number;
+  username: string;
+  password: string;
+  email: string;
+  role: string;
+
+  public displayedColumns = ['id', 'username', 'email', 'role', 'update', 'delete'];
   public dataSource = new MatTableDataSource<User>();
   public users: User[];
   public editUser: User;
   public deleteUser: User;
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
- 
+  private updateUserDialog = UpdateUserDialog;
 
   constructor(private userService: UserService, public dialog: MatDialog) { }
 
@@ -60,7 +73,22 @@ export class BoardAdminComponent implements OnInit, AfterViewInit {
     );
   }
 
-  public onDeleteUser(userId: number): void {
+  public addUser(addForm: NgForm): void {
+    document.getElementById('add-graduated-form').click();
+    this.userService.addUser(addForm.value).subscribe(
+      (response: User) => {
+        console.log(response);
+        this.getUsers();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public deleteItem(userId: number): void {
     this.userService.deleteUser(userId).subscribe(
       (response: void) => {
         console.log(response);
@@ -73,7 +101,15 @@ export class BoardAdminComponent implements OnInit, AfterViewInit {
   }
 
   openDialog() {
-    this.dialog.open(UpdateUserDialog);
+    this.dialog.open(UpdateUserDialog, {
+      data: {id: this.id, username: this.username, password: this.password, email: this.email, role: this.role}
+    });
+  }
+
+  public editRecord(id, username, password, email, role) {
+    this.dialog.open(this.updateUserDialog, {
+      data: {id: id, username: username, password: password, email: email, role: role, dataSource: this.dataSource}
+    });
   }
 }
 
@@ -82,4 +118,76 @@ export class BoardAdminComponent implements OnInit, AfterViewInit {
   selector: 'board-admin-update-panel',
   templateUrl: 'board-admin-update-panel.html',
 })
-export class UpdateUserDialog {}
+export class UpdateUserDialog {
+  constructor(private dropdownService: UserService, public dialogRef: MatDialogRef<UpdateUserDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: User, public userService: UserService) {}
+
+    public dataSource = new MatTableDataSource<User>();
+    public users: User[];
+    selectedValue = this.data.role;
+    hide = true;
+    rolesList;
+
+    public roles: Role[];
+
+formControl = new FormControl('', [
+Validators.required
+// Validators.email,
+]);
+
+ngOnInit() {
+  this.getUsers();
+
+  this.dropdownService.getRoles().subscribe(rolesList => {
+    this.rolesList = rolesList;
+  });
+}
+
+public getUsers(): void {
+  this.userService.getUsers().subscribe(
+    (response: User[]) => {
+      this.users = response;
+    },
+    (error: HttpErrorResponse) => {
+      alert(error.message);
+    }
+  );
+}
+
+getErrorMessage() {
+return this.formControl.hasError('required') ? 'Required field' :
+this.formControl.hasError('email') ? 'Not a valid email' :
+'';
+}
+
+submit() {
+// emppty stuff
+}
+
+onNoClick(): void {
+this.dialogRef.close();
+}
+
+stopEdit(): void {
+this.userService.updateUser(this.data);
+}
+
+public updateUser(user: User): void {
+   this.userService.updateUser(user).subscribe(
+    (response: User) => {
+      console.log(response);
+      this.getUsers();
+     },
+     (error: HttpErrorResponse) => {
+      alert(error.message);
+     }
+   );
+}
+
+  GetRoleById(event) {
+    this.dropdownService.findRole(Number(event.target.value)).subscribe(rolesList => {
+      this.rolesList = rolesList;
+    });
+  }
+
+}
