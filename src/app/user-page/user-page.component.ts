5ce59e1468f1db5bb6d3267ab43673229431ee2d
Roman Sulymka <roma.sulimak@gmail.com>
2021-05-13 00:01:45 +0300

    Modify User Page, Remove all permisions for users

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,70 +1,73 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { User } from './user';
-import { UserService } from './user.service';
+import { Component, OnInit, ViewChild, AfterViewInit, Inject, ViewEncapsulation } from '@angular/core';
+import { UserService } from '../user-page/user.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator'
+import { User } from '../user-page/user';
 import { HttpErrorResponse } from '@angular/common/http';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { Graduated } from '../graduated/graduated';
+import { GraduatedService } from '../graduated/graduated.service';
 import { AuthService } from '../_services/auth.service';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-user-page',
   template: `<h3>User Page</h3>`,
-  templateUrl: './user-page.component.html'
+  templateUrl: './user-page.component.html',
+  styleUrls: ['./user-page.component.css']
 })
 export class UserPageComponent implements OnInit, AfterViewInit {
-  public users: User[];
-  public editUser: User;
-  public deleteUser: User;
-  public findUser: User;
+  id: number;
+  username: string;
+  password: string;
+  email: string;
+  role: string;
+  works: string;
+  nav_position: string = 'end';
   public displayedColumns = ['id', 'username', 'email', 'role'];
   public dataSource = new MatTableDataSource<User>();
+  public dataSourceGraduate = new MatTableDataSource<Graduated>();
+  public users: User[];
+  public deleteUser: User;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  form: any = {
-    username: null,
-    email: null,
-    password: null
-  };
-  isSuccessful = false;
-  isSignUpFailed = false;
-  errorMessage = '';
-  
-  constructor(private userService: UserService, private authService: AuthService) { }
+  constructor(private userService: UserService, public dialog: MatDialog) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.getUsers();
   }
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-  }
-
   public getUsers(): void {
-    this.userService.getUsers().subscribe(
-      (response: User[]) => {
-        this.users = response;
-      },
-      (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    );
+    this.userService.getUsers()
+    .subscribe(res => {
+      this.dataSource.data = res as User[];
+    })
   }
 
-  public onAddUser(addForm: NgForm): void {
-    document.getElementById('add-user-form').click();
-    this.userService.addUser(addForm.value).subscribe(
-      (response: User) => {
-        console.log(response);
-        this.getUsers();
-        addForm.reset();
-      },
-      (error: HttpErrorResponse) => {
-        alert(error.message);
-        addForm.reset();
-      }
-    );
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.dataSourceGraduate.sort = this.sort;
+    this.dataSourceGraduate.paginator = this.paginator;
+  }
+
+  public customSort = (event) => {
+    console.log(event);
+  }
+
+  public customSortGraduate = (event) => {
+    console.log(event);
+  }
+
+  public doFilter = (value: string) => {
+    this.dataSource.filter = value.trim().toLocaleLowerCase();
+  }
+
+  public doFilterGraduate = (value: string) => {
+    this.dataSourceGraduate.filter = value.trim().toLocaleLowerCase();
   }
 
   public onUpdateUser(user: User): void {
@@ -79,7 +82,22 @@ export class UserPageComponent implements OnInit, AfterViewInit {
     );
   }
 
-  public onDeleteUser(userId: number): void {
+  public addUser(addForm: NgForm): void {
+    document.getElementById('add-graduated-form').click();
+    this.userService.addUser(addForm.value).subscribe(
+      (response: User) => {
+        console.log(response);
+        this.getUsers();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public deleteItem(userId: number): void {
     this.userService.deleteUser(userId).subscribe(
       (response: void) => {
         console.log(response);
@@ -87,76 +105,6 @@ export class UserPageComponent implements OnInit, AfterViewInit {
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
-      }
-    );
-  }
-
-  public onFindUser(userId: User): void {
-    this.userService.findUser(userId).subscribe(
-      (response: User) => {
-        console.log(response);
-        this.getUsers();
-      },
-      (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    );
-  }
-
-  public searchUsers(key: string): void {
-    console.log(key);
-    const results: User[] = [];
-    for (const user of this.users){
-      if (user.username.toLowerCase().indexOf(key.toLowerCase()) !== -1
-      || user.email.toLowerCase().indexOf(key.toLowerCase()) !== -1){
-        results.push(user);
-      }
-    }
-    this.users = results;
-    if (results.length === 0 || !key){
-      this.getUsers();
-    }
-  }
-
-  public onOpenModal(user: User, mode: string): void {
-    const container = document.getElementById('main-container');
-    const button = document.createElement('button');
-    button.type = 'button';
-    button.style.display = 'none';
-    button.setAttribute('data-toggle', 'modal');
-    if (mode === 'add'){
-     button.setAttribute('data-target', '#addUserModal');
-    }
-
-    if (mode === 'edit'){
-      this.editUser = user;
-      button.setAttribute('data-target', '#updateUserModal');
-    }
-
-    if (mode === 'delete'){
-      this.deleteUser = user;
-      button.setAttribute('data-target', '#deleteUserModal');
-    }
-    if (mode === 'find'){
-      this.findUser = user;
-      button.setAttribute('data-target', '#findUserModal');
-    }
-    container.appendChild(button);
-    button.click();
-  }
-
-  onSubmit(): void {
-    const { username, email, password } = this.form;
-
-    this.authService.register(username, email, password).subscribe(
-      data => {
-        console.log(data);
-        this.isSuccessful = true;
-        this.isSignUpFailed = false;
-      },
-      err => {
-        this.errorMessage = err.error.message;
-        this.isSignUpFailed = true;
       }
     );
   }
